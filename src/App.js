290acdc6d792764084b5c2468f25283e6285a28c
saif maamun <sadifmamun@gmail.com>
2022-08-30 11:13:51 +0600

    2.3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
     return (
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-                Simple Counter Application 2.2
+                Simple Counter Application 2.3
             </h1>
 
             <div className="max-w-md mx-auto mt-10 space-y-5">
