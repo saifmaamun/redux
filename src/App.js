6b8c227fd234384817557ed10497e2f099a36b1d
saif maamun <sadifmamun@gmail.com>
2022-08-30 11:51:42 +0600

    store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Provider } from "react-redux";
 import Counter from "./components/Counter";
+import store from "./redux/store";
 
 export default function App() {
     return (
+        <Provider store={store}>
+
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                 Simple Counter Application 2.3
@@ -13,5 +17,6 @@ export default function App() {
                 <Counter />
             </div>
         </div>
+        </Provider>
     );
 }
