4ed6765001b77325589205b493496b7e32c5926f
saifmaamun <sadifmamun@gmail.com>
2022-11-17 20:29:02 +0600

    basic done

--- script.js
+++ script.js
@@ -1,48 +1,125 @@
 // get dom elements
+const containerEl = document.getElementById('container');
 const counterEl = document.getElementById('counter');
-const incrementEl = document.getElementById('increment');
-const decrementEl = document.getElementById('decrement');
+const addCounterEl = document.getElementById('addcounter');
+const resetCounterEl = document.getElementById('resetcounter');
+const resetValueEl = document.getElementById('resetvalue');
+
 
 // action identifier
 const INCREMENT = 'increment';
 const DECREMENT = 'decrement';
+const ADDCOUNTER ='addcounter';
+const RESETCOUNTER = 'resetcounter';
+const RESETVALUE = 'resetvalue';	
 
 // initial state
-const initialState = {
+const initialState =[
+     {
     value: 0,
-}
+    id: 1,
+},
+]
 
 // action creator
-const increment = (value) => {
+const increment = (counterId) => {
+
     return {
         type: INCREMENT,
-        payload: value,
+        payload: {
+            id:counterId,
+        }
     }
 }
     
-const decrement = (value) => {
+const decrement = (counterId) => {
     return {
         type: DECREMENT,
-        payload: value,
+        payload:{
+            id: counterId,
+        } 
     }
+}
+
+const addcounter=()=>{
+    return {
+        type: ADDCOUNTER,
     }
+}
 
+const resetcounter=()=>{
+    return {
+        type: RESETCOUNTER,
+    }
+}
 
+const resetvalue=()=>{
+    return {
+        type: RESETVALUE,
+    }
+}
+
+// functions
+const incrementHandler=(id)=>{
+    store.dispatch(increment(id))
+}
+const decrementHandler=(id)=>{
+    store.dispatch(decrement(id))
+}
 
 // reducer function
 function counterReducer(state = initialState, action) {
     
     if (action.type === INCREMENT) {
-        return {
-            ...state,
-            value: state.value + action.payload,
-        };
+        const {id} = action.payload
+        return state.map((counter)=>{
+            if (counter.id ===id){
+                return {
+                   ...counter,
+                    value: counter.value + id,
+            }
+        }
+            else return {
+                ...counter
+            }
+        })
     }
-    else if (action.type === DECREMENT) {
-        return {
-            ...state,
-            value: state.value - action.payload,
-        };
+    if (action.type === DECREMENT) {
+        const {id} =action.payload
+        return state.map((counter)=>{
+            if (counter.id ===id){
+                return {
+                    ...counter,
+                    value: counter.value - id,
+            }
+        }
+            else return {
+               ...counter
+            }
+
+        })
+    }
+    if (action.type === ADDCOUNTER) {
+    return [
+        ...state,
+        {
+            value: 0,
+            id: state.length+1,
+        }
+    ]
+    }
+    if (action.type === RESETCOUNTER) {
+    return state=initialState
+    }
+    if (action.type === RESETVALUE) {
+        const id =action.payload
+        return state.map((counter)=>{
+            
+                return {
+                        ...counter,
+                         value: 0,
+            }
+        })
     }
     else {
         return state;
@@ -56,17 +133,45 @@ const store = Redux.createStore(counterReducer);
 // update ui
 const render = () => {
     const state = store.getState();
-    counterEl.innerText = state.value.toString();
+    let innerDiv=""
+    state.forEach((counter) => {
+        innerDiv +=`
+        <div
+          class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+        >
+          <div id=${counter.id} class="text-2xl font-semibold">${counter.value}</div>
+          <div class="flex space-x-3">
+            <button
+              class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+              onClick="incrementHandler(${counter.id})"
+            >
+              Increment
+            </button>
+            <button
+              class="bg-red-400 text-white px-3 py-2 rounded shadow"
+              onClick="decrementHandler(${counter.id})"
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
+        `
+    })
+    containerEl.innerHTML =innerDiv;
+        
 }
+render();
 
 store.subscribe(render);
 
 // button lick listener
 
-incrementEl.addEventListener('click', () => {
-    store.dispatch(increment(13))
-});
-
-decrementEl.addEventListener('click', () => {
-    store.dispatch(decrement(9))
+addCounterEl.addEventListener('click', () => {
+    store.dispatch(addcounter())
+})
+resetCounterEl.addEventListener('click', () => {
+    store.dispatch(resetcounter())
+})
+resetValueEl.addEventListener('click', () => {
+    store.dispatch(resetvalue())
 })
