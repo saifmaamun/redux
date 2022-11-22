0ecbe66f3fae744b6c459363778c5010721d72c8
saifmaamun <sadifmamun@gmail.com>
2022-11-22 22:05:40 +0600

    2nd

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,10 @@ import { createStore, applyMiddleware} from "redux";
 import rootReducer from "./rootReducer";
 import myLogger from "../redux/middlewears/myLogger"
 import logger from "redux-logger"
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
 
-const store = createStore(rootReducer,applyMiddleware(logger,myLogger));
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,myLogger)));
 
 export default store;
