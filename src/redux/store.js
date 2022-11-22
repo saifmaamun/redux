80809954843b02de2ee99341de20c818e43cc024
saifmaamun <sadifmamun@gmail.com>
2022-11-22 17:00:45 +0600

    2nd life

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware} from "redux";
 import rootReducer from "./rootReducer";
 
 
-const store = createStore(rootReducer)
+const myLogger=(store)=>(next)=>(action)=>{
+    console.log(`ACTION : ${JSON.stringify(action)}`);
+    console.log(`BEFORE : ${JSON.stringify(store.getState())}`);
+    return next(action);
+}
+
+
+const store = createStore(rootReducer,applyMiddleware(myLogger))
 
 export default store;
